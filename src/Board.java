bd258c1c5aff8f30a0e70f16771bbba8925e8614
Ethan Reed <reedx500@umn.edu>
2020-02-27 17:06:41 -0600

    Need to fix while loop

--- src/Board.java
+++ src/Board.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
 
 public class Board {
+    private int[][] spaces;
 
     public Board(){
+        if (this.gameplayDeterminer() == "standard"){
+            this.spaces = new int[8][8];
+        } else{
+            this.spaces = new int[12][12];
+        }
     }
 
-    public String ScannerInput(){
+    public String gameplayDeterminer(){
         Scanner input = new Scanner(System.in);
 
         String gameChoice = "";
 
-        while (!(gameChoice.toLowerCase().equals("standard") || !gameChoice.toLowerCase().equals("expert"))){
+        int determinedChoice = 1;
 
+        while (determinedChoice != 0){
+            System.out.println("Enter a play style: Standard or Expert");
+            gameChoice = input.nextLine();
+            if((gameChoice.toLowerCase().equals("standard") || !gameChoice.toLowerCase().equals("expert"))){
+                determinedChoice = 0;
+            }
         }
         return gameChoice;
+    }
+
+    public static void main(String[] args) {
+        Board A = new Board();
     }
 }
